48f8cafb11e2e136d0fedcaa9674434973b13af0
Henrik3 <henriqlh@gmail.com>
2023-08-07 10:25:15 -0300

    update

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ app.exception_handler(Exception)(Error.exceptionHandler)
 # Bind to all available network interfaces on port 8000
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
